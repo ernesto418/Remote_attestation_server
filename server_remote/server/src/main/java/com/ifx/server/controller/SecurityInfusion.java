2706e80d6c8b5a36a1a48d621eb747ece598bb6f
José Luis Tejero López <39380561+joseluistejero@users.noreply.github.com>
2022-03-01 12:56:36 +0100

    Updated relative path

--- server_remote/server/src/main/java/com/ifx/server/controller/SecurityInfusion.java
+++ server_remote/server/src/main/java/com/ifx/server/controller/SecurityInfusion.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.json.JSONObject;
+import java.nio.file.Paths;
 
 public class SecurityInfusion 
 {
@@ -15,9 +16,10 @@ public class SecurityInfusion
 	properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 	properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 	properties.put("security.protocol","SSL");
-	properties.put("ssl.truststore.location","/home/pi/Desktop/RemoteAttestation/Remote_attestation_server-Server_always_yes/server_remote/server/src/main/resources/certificatesKafka/truststore.jks");
+	String currentDir = System.getProperty("user.dir");
+	properties.put("ssl.truststore.location",currentDir+"/../"+"src/main/resources/certificatesKafka/truststore.jks");
 	properties.put("ssl.truststore.password","kafka123");
-	properties.put("ssl.keystore.location","/home/pi/Desktop/RemoteAttestation/Remote_attestation_server-Server_always_yes/server_remote/server/src/main/resources/certificatesKafka/keystore.jks");
+	properties.put("ssl.keystore.location",currentDir+"/../"+"src/main/resources/certificatesKafka/keystore.jks");
 	properties.put("ssl.keystore.password","kafka123");
 	properties.put("ssl.key.password","kafka123");
 	KafkaProducer<String,String> myProducer= new KafkaProducer<String,String>(properties);
